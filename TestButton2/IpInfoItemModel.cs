7400fb3e9976f13e4f4b6f551b0804a2fc674e73
Saamer Mansoor <i@saamer.me>
2017-10-20 11:22:43 -0600

    First Commit - Working App
    Edited the existing Base template from Xamarin Android to build an app that just adds 3 steps in order to build your own app.
    You can use a Json to C# converter website to conver edit the Model needed to use the data within the app.
    Alsom you could use a JsonViewer extension on your Google Chrome to ensure that your API spits data in proper JSON format that you can use to replace the ipinfo REST service.

---
+++ TestButton2/IpInfoItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TestButton2
+{
+    public class IpInfoItemModel
+    {
+        public string ip { get; set; }
+        public string hostname { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public string loc { get; set; }
+        public string org { get; set; }
+        public string postal { get; set; }
+    }
+}
---
+++ TestButton2/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Support.Wearable.Views;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestButton2
+{
+    [Activity(Label = "TestButton2", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var v = FindViewById<WatchViewStub>(Resource.Id.watch_view_stub);
+            v.LayoutInflated += delegate
+            {
+
+                // Get our button from the layout resource,
+                // and attach an event to it
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+
+                button.Click += async delegate
+                {
+                    var notification = new NotificationCompat.Builder(this)
+                        .SetContentTitle("Button tapped")
+                        .SetContentText($"Button tapped {count++} times!")
+                        .SetSmallIcon(Android.Resource.Drawable.StatNotifyVoicemail)
+                        .SetGroup("group_key_demo").Build();
+
+                    var manager = NotificationManagerCompat.From(this);
+                    manager.Notify(1, notification);
+
+                    HttpClient client = new HttpClient();
+                    Uri uri = new Uri("http://ipinfo.io/json");
+                    string obstring = await client.GetStringAsync(uri);
+                    IpInfoItemModel info = JsonConvert.DeserializeObject<IpInfoItemModel>(obstring);
+
+                    button.Text = "Check Notification!";
+                    button.Text = info.city;
+                };
+            };
+        }
+    }
+}
+
+
